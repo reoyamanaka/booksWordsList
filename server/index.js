b47c5f7d80b2d710f7657fc439a028b86f731333
Reo Yamanaka <ryamanaka807@gmail.com>
2021-04-17 19:45:19 -0400

    set up server port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
+const PORT = process.env.PORT || 3001;
+
 
 app.use(cors());
 app.use(express.json());
 
+const db = mysql.createConnection( {
+    user: "your_username",
+    host: "localhost",
+    password: "your_password",
+    database: "words"
+});
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
